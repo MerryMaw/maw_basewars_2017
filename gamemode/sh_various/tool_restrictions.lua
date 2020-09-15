a18da089fabf7fd13b9e13728bb59bfdf5ba3b2d
MerryMaw <cjbremer@gmail.com>
2020-09-15 18:00:03 +0200

    weird situations with terrible

--- gamemode/sh_various/tool_restrictions.lua
+++ gamemode/sh_various/tool_restrictions.lua
@@ -4,7 +4,17 @@ local RestrictedTools = {
 
 function GM:CanTool( ply, tr, tool )
 	if (SERVER) then
-		if (IsValid(tr.Entity) and tr.Entity:GetCreator() != ply) then return false end
+		if (IsValid(tr.Entity)) then
+			if (tr.Entity:CreatedByMap()) then
+				return false
+			elseif (IsValid(tr.Entity:GetCreator())) then 
+				if (tr.Entity:GetCreator() != ply) then
+					return false 
+				end
+			else
+				tr.Entity:SetCreator(ply)
+			end
+		end
 	end
 	
 	return ply:IsAdmin() or !table.HasValue(RestrictedTools,tool:lower())
